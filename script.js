d3795f15b2865d5e8ffe16a5ac9c55e88f36fdb2
Corey Donahue <137344214+CDarts48@users.noreply.github.com>
2023-11-28 18:58:54 -0700

    Update script.js with api keys and code frames

--- script.js
+++ script.js
@@ -99,7 +99,46 @@ function authorBio() {
 
 authorBio();
 
+// const apiKey = "AIzaSyAg_vIE7Oue7sQBYZR4tScNEwAtvAJdFNg";
+// console.log(apiKey);
 
+// Google Books API call
+// sintax for the api call note saves app size
+// Option 2: Use gapi.client.request
+// A more general way to make requests is to use gapi.client.request. Your application does not have to load the Discovery Document as in the first option, but it must still set the API key (and auth for some APIs). While you need to manually fill in REST parameters with this option, it saves one network request and reduces application size.
+
+// <script src="https://apis.google.com/js/api.js"></script>
+// <script>
+// function start() {
+//   // 2. Initialize the JavaScript client library.
+//   gapi.client.init({
+//     'apiKey': 'YOUR_API_KEY',
+//     // clientId and scope are optional if auth is not required.
+//     'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
+//     'scope': 'profile',
+//   }).then(function() {
+//     // 3. Initialize and make the API request.
+//     return gapi.client.request({
+//       'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
+//     })
+//   }).then(function(response) {
+//     console.log(response.result);
+//   }, function(reason) {
+//     console.log('Error: ' + reason.result.error.message);
+//   });
+// };
+// // 1. Load the JavaScript client library.
+// gapi.load('client', start);
+// </script>
+
+// for LOC API CALL
+// // Const = "Year" (change year to input value)
+// fetch(`https://www.loc.gov/search/?q=books&fo=json&dates=${year}`)
+//   .then(response => response.json())
+//   .then(data => {parse data in to our app}
+//   console.log(data);
+//     )
+//     .catch(error => console.log(error));
 
 
 
